5bf4c5e4bbfe3d58bdb6d95de686a72699795657
Keny Jolondek <keny.jolondek@iatec.com>
2020-07-05 22:56:49 -0300

    Added component SQL Query Builder for INSERT from Excel file

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -3,15 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home.component';
+import { SqlQueryBuilderComponent } from '../sql-query-builder/sql-query-builder.component';
 
 const routes: Routes = [
-  {
-      path: '', component: HomeComponent,
-  },
+  { path: '', component: HomeComponent },
+  { path: 'sql-query-builder', component: SqlQueryBuilderComponent },
 ];
 
 const myComponents = [
-  HomeComponent
+  HomeComponent,
+  SqlQueryBuilderComponent
 ];
 
 @NgModule({
